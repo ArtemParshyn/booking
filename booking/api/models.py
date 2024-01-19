6431bcec8f7acff5f9505e709497ee91ce6f2ef0
Артем <parshyn4@gmail.com>
2024-01-19 23:25:22 +0300

    created migrations and models

--- booking/api/models.py
+++ booking/api/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+choices = [
+    ("F", "free"),
+    ("R", "reserved")
+]
+
+
+class User(AbstractUser):
+    ...
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=64)
+    street = models.CharField(max_length=64)
+    cost = models.IntegerField()
+    status = models.CharField(choices=choices)
+
+
+class Booking(models.Model):
+    name = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    start = models.DateField()
+    end = models.DateField()
